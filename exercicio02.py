ec496ce434329273b514f6abf5094139997bb201
pedromarques03 <master>
2025-03-31 09:50:29 -0300

    enviando exercicio02

---
+++ exercicio02.py
@@ -0,0 +1,7 @@
+numero1= int(input ("Digite o primeiro numero:"))
+numero2= int(input ("Digite o segundo numero:"))
+soma = numero1+numero2
+subtracao = numero1-numero2
+multiplicacao = numero1*numero2
+divisao = numero1/numero2
+print (f"a soma desses dois numeros e {soma}, a subtracao desses dois numeros e {subtracao}, a multiplicacao desses dois numeros e {multiplicacao} e a divisao desses dois numeros e {divisao}.")
