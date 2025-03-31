a79c558433b296d7e0ef6f2fb4a1456ceb41a1c8
pedromarques03 <master>
2025-03-31 09:15:44 -0300

    enviando exercicio01

--- exercicio01.py
+++ exercicio01.py
@@ -1,1 +1,7 @@
-print()
+nome = input("Digite seu nome:")
+idade =int(input  ("Digite sua idade:"))
+salario =float(input ("Digite seu salario:"))
+percentual = float(input ("Digite o percentual:"))
+aumento = salario*percentual/100
+novosalario = salario+aumento
+print (f"{nome}, vote tem {idade} anos. seu salario atual {salario}, voce teve {aumento} de aumento e seu novo salario sera{novosalario:.2f}")
